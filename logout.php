274ec667b790e2002799bfdfdcc4cd52677d8c01
rferrucci <ronald.ferrucci@gmail.com>
2017-01-15 19:39:36 -0600

    Update logout.php
    logs user restaurants out of the database

--- logout.php
+++ logout.php
@@ -1,5 +1,6 @@
 <?php
 //built for client's food delivery service app, where restaurants recieve orders from her WordPress installation
+//logs them out of the database
 
 //connect to wordpress database
 require_once('db_file.php');
@@ -9,7 +10,7 @@ $con = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME) or die('Unable to Con
 if($_SERVER['REQUEST_METHOD']=='POST'){
 
 	$email = $_POST['email'];
-  //client wanted to know when restaurants were online
+  	//client wanted to know when restaurants were online, online is 1, offline is 0
 	$sql = "UPDATE wp_restaurants SET online=0 WHERE email = ?";
 	$stmt = $con->prepare($sql);
 	$stmt-> bind_param('s', $email);
