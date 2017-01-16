9f263d43a871fa9e110f17042aa6dbf5cabccbb5
rferrucci <ronald.ferrucci@gmail.com>
2017-01-15 19:55:44 -0600

    Update db_file.php

--- db_file.php
+++ db_file.php
@@ -1,6 +1,16 @@
 <?php
 
-//connects non-wordpress php files to wordpress database
+/**
+ * db_file.php
+ *
+ * Connects app to users WordPress database
+ *
+ * @author     Ronald R. Ferrucci
+ * @copyright  2017 Ronald R. Ferrucci
+ * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+
+ */
+
 $config_file = $_SERVER['DOCUMENT_ROOT'] . '/wp-config.php';
 require_once $config_file;
 ?>
