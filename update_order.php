11237d7d7bfd3e1c42ccdf1ec6b05196b806f229
rferrucci <ronald.ferrucci@gmail.com>
2017-01-15 19:54:19 -0600

    Update update_order.php

--- update_order.php
+++ update_order.php
@@ -1,9 +1,18 @@
 <?php
 
-/*
-This script updates the order in the database to reflect change of status from the app user, either setting status to "recived," acknowledging 
+/**
+ * update_order.php
+ *
+ * This script updates the order in the database to reflect change of status from the app user, either setting status to "recived," acknowledging 
 receipt of the order, or "completed," indicated that the order has been filled. Also adds a note from the restaurant, if there is one.
-*/
+ *
+ * @author     Ronald R. Ferrucci
+ * @copyright  2017 Ronald R. Ferrucci
+ * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+
+ */
+
+
 include_once('db_file.php');
 $con=mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
 
