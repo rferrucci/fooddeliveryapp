f292ccddc04ef891a2833c37851a9e9d1bf99f79
rferrucci <ronald.ferrucci@gmail.com>
2017-03-30 18:34:27 -0500

    Create restaurant-changes.php

---
+++ restaurant-changes.php
@@ -0,0 +1,35 @@
+<?php
+require_once('db_file.php');
+$con = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+if (mysqli_connect_errno($con))
+{
+   echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+$con->set_charset("utf8");
+
+include_once("getRestaurants.php");
+
+if (isset($_POST)){
+	if (isset($_POST['ID'])){
+		$id = $_POST['ID'];
+		}
+	if ($_POST['submitChanges'] == 'delete'){
+		$restaurants[$id]->delete();
+		
+	}
+	else if ($_POST['submitChanges'] == 'update'){
+		$restaurants[$id]->update($_POST['email'],$_POST['restaurant']);
+		$email = trim($_POST['email']);
+		$restaurant = $_POST['restaurant'];
+		
+	}
+	else if ($_POST['submitChanges'] == 'insert'){
+		$res=new Restaurant($_POST['email'],$_POST['restaurant']);
+		$res->insert();
+		$restaurants[$res->id] = $res;
+		$email = trim($_POST['email']);
+		$restaurant = $_POST['restaurant'];
+		
+	}
+}
+$con->close();
