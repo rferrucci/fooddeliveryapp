6d07143120a3c28e95e7cd2c534b632cbe3de57f
rferrucci <ronald.ferrucci@gmail.com>
2017-03-31 19:22:48 -0500

    Update restaurant-changes.php

--- restaurant-changes.php
+++ restaurant-changes.php
@@ -1,4 +1,5 @@
 <?php
+// process-restaurant.php
 require_once('db_file.php');
 $con = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
 if (mysqli_connect_errno($con))
@@ -8,6 +9,36 @@ if (mysqli_connect_errno($con))
 $con->set_charset("utf8");
 
 include_once("getRestaurants.php");
+$errors         = array();      // array to hold validation errors
+$data           = array();      // array to pass back data
+
+// validate the variables ======================================================
+    // if any of these variables don't exist, add an error to our $errors array
+
+    if (empty($_POST['restaurant']))
+        $errors['name'] = 'Name is required.';
+
+
+// return a response ===========================================================
+
+    // if there are any errors in our errors array, return a success boolean of false
+    if ( ! empty($errors)) {
+
+        // if there are items in our errors array, return those errors
+        $data['success'] = false;
+        $data['errors']  = $errors;
+    } else {
+
+        // if there are no errors process our form, then return a message
+
+        // DO ALL YOUR FORM PROCESSING HERE
+        // THIS CAN BE WHATEVER YOU WANT TO DO (LOGIN, SAVE, UPDATE, WHATEVER)
+
+        // show a message of success and provide a true success variable
+        $data['success'] = true;
+        $data['message'] = 'Success!';
+    }
+
 
 if (isset($_POST)){
 	if (isset($_POST['ID'])){
@@ -20,8 +51,7 @@ if (isset($_POST)){
 	else if ($_POST['submitChanges'] == 'update'){
 		$restaurants[$id]->update($_POST['email'],$_POST['restaurant']);
 		$email = trim($_POST['email']);
-		$restaurant = $_POST['restaurant'];
-		
+		$restaurant = $_POST['restaurant'];		
 	}
 	else if ($_POST['submitChanges'] == 'insert'){
 		$res=new Restaurant($_POST['email'],$_POST['restaurant']);
@@ -33,3 +63,6 @@ if (isset($_POST)){
 	}
 }
 $con->close();
+
+    // return all our data to an AJAX call
+    echo json_encode($data);
