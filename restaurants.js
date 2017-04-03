f53f5d8690ff509e27bad78748f5b7054263b916
rferrucci <ronald.ferrucci@gmail.com>
2017-04-03 16:28:40 -0500

    Create restaurants.js

---
+++ restaurants.js
@@ -0,0 +1,38 @@
+/*
+JavaScript/Jquery code for inserting, removing, or updating entries in the table. In the restaurants.php file, I actually have the jQuery
+code implemented using php for each function since I also have each function coded as a method in a restaurants class. Just wanted to
+include it here as straight JQuery.
+*/
+
+$(document).ready(function(){
+  $('form').submit(function(event){
+    var restaurant = $('input[name=restaurant]').val(); 
+    var email = $('select[name=email]').val(); 
+    var submit = $('button[name=submitChanges]').val(); 
+    var ID = $('input[name=ID]').val(); 
+    
+    if (submit=='delete') $('#rid-' + ID).remove();
+    else if (submit=='update'){
+    	$("#rid-" + ID ).find(".restaurant").html(restaurant);
+	$("#rid" + ID ).find(".email").html(email);
+	}
+    else if (submit=='insert'){
+    	var link = $(location).attr('href');
+	var new_row ='<tr id="rid-' + ID + '">';
+	new_row +='<td><a href ="' + link + '?action=edit&id=' + ID + '">Edit</a><br>';
+	new_row += '<a href ="' + link + '?action=delete&id=' + ID + '">Delete</a>';
+	new_row += '<td class="restaurant">' + restaurant + '</td>';
+	new_row += '<td class="email">' + email +'</td>';
+	new_row += '<td class="online"><img width="16" height="16"  alt="offline" title="offline"'; 
+	new_row += 'src="images/transparent-red.png"</td></tr>';
+	$('#restaurantTable tr:last').after(new_row);
+			//$("#" + rid).(".restaurant").html(restaurant);
+			//$("#" + rid).(".email").html(email);
+    
+    	}
+
+    // stop the form from submitting and refreshing
+    event.preventDefault();
+
+  });
+});
