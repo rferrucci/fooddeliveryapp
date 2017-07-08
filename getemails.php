f83f04cf7af2fee16b4b04753dfd348981c836ac
rferrucci <ronald.ferrucci@gmail.com>
2017-07-07 19:20:17 -0500

    Update getemails.php

--- getemails.php
+++ getemails.php
@@ -7,12 +7,16 @@ $query = "SELECT ID, user_email
 	JOIN wp_usermeta on wp_users.ID=wp_usermeta.user_id 
 	WHERE wp_usermeta.meta_key='wp_capabilities' 
 	AND wp_usermeta.meta_value LIKE '%shop_manager%'";
-$shop_managers = $wpdb->get_results($query);
-				
+#$shop_managers = $wpdb->get_results($query);
+$stmt = $con->prepare($query);
+$stmt->execute();		
+$shop_managers = $stmt->get_result();		
 //we also want to get emails that are already associated with restaurants
 $query = "SELECT email FROM wp_restaurants";
-$results = $wpdb->get_results($query);
-
+#$results = $wpdb->get_results($query);
+$stmt = $con->prepare($query);
+$stmt->execute();
+$results = $stmt->get_result();
 $used = array();
 foreach ($results as $r){
 	array_push($used, $r->email);
@@ -21,10 +25,10 @@ foreach ($results as $r){
 $emails = array();
 
 foreach ($shop_managers as $s){
-	$email = $s->user_email;
-	$id = $s->ID;
-	if (in_array($email, $used)){ $s->disabled = 'yes'; }
-	else { $s->disabled = 'no'; }
+	$email = $s['user_email'];
+	$id = $s['ID'];
+	if (in_array($email, $used)){ $s['disabled'] = 'yes'; }
+	else { $s['disabled'] = 'no'; }
 	
 	#$user = array('id'=>$id, 'email'=>$email, 'selected'=>$selected)
 	array_push($emails, array('id'=>$id, 'email'=>$email, 'disabled'=>$disabled));
